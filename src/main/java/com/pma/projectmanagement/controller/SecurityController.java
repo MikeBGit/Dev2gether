97f15542b8e8caf8e6d1584c3916cd5e32abd0b0
AnthonyFSD-02 <91700433+AnthonyFSD-02@users.noreply.github.com>
2022-05-29 13:25:14 -0400

    Thymeleaf Extra SpringSecurity Implemented as well as login, logout, dashboard and main views fixed projects page

--- src/main/java/com/pma/projectmanagement/controller/SecurityController.java
+++ src/main/java/com/pma/projectmanagement/controller/SecurityController.java
@@ -58,7 +58,7 @@ public class SecurityController {
         if (auth != null){
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login?logout";
+        return "security/logout";
     }
 
     @GetMapping("/dashboard")
