efbf72463d2fe397b035f5416987d913912d4c13
MikeBGit <91700400+MikeBGit@users.noreply.github.com>
2022-06-02 15:16:50 -0400

    Final Clean v2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,32 +1,20 @@
 
---# insert into user_accounts (user_id, enabled, email, password, role, username ) values (10,1,'a@gmail.com','pass', 'ADMIN', 'admin');
-
--- INSERT EMPLOYEES
---insert into user (id, first_name, last_name, email, password, username, enabled) values (10, 'John', 'Warton', 'warton@gmail.com', "password123", "JohnW", true);
-
-
---insert into project (id, name, stage, description, owner_id) values (11, 'New Employee Budget',  'COMPLETED', 'Decide on a new employee bonus budget for the year and figureout who will be promoted', 10);
---insert into project (id, name, stage, description, owner_id) values (12, 'Office Reconstruction', 'INPROGRESS', 'The office building in Monroe has been damaged due to hurricane in the region. This needs to be reconstructed', 10);
---insert into project (id, name, stage, description, owner_id) values (13, 'Improve Intranet Security', 'INPROGRESS', 'With the recent data hack, the office security needs to be improved and proper security team needs to be hired for implementation', 10);
-
--- INSERT PROJECT_EMPLOYEE_RELATION (Removed duplicates from video)
---insert into project_user (user_id, project_id) values (1,1000);
---insert into project_user (user_id, project_id) values (1,1001);
---insert into project_user (user_id, project_id) values (1,1002);
---insert into project_user (user_id, project_id) values (3,1000);
---insert into project_user (user_id, project_id) values (6,1002);
---insert into project_user (user_id, project_id) values (6,1003);
+INSERT INTO dev2gether.user (id, email, enabled, first_name, last_name, password, role) VALUES (1000, 'reza@johnabbottcollege.net', 1, 'Reza', 'Shalchian', '$2a$10$rFK3.d/QbOaOGLxWweuimuob6om5j0PKeJOShGBrFLOjcfVwmUUV.','ADMIN');
+INSERT INTO dev2gether.user (id, email, enabled, first_name, last_name, password, role) VALUES (1001, 'mike@johnabbottcollege.net', 1, 'Michael', 'Baldessari', '$2a$10$rFK3.d/QbOaOGLxWweuimuob6om5j0PKeJOShGBrFLOjcfVwmUUV.','ADMIN');
+INSERT INTO dev2gether.user (id, email, enabled, first_name, last_name, password, role) VALUES (1002, 'anthony@johnabbottcollege.net', 1, 'Anthony', 'Aboussafy', '$2a$10$rFK3.d/QbOaOGLxWweuimuob6om5j0PKeJOShGBrFLOjcfVwmUUV.','ADMIN');
+INSERT INTO dev2gether.user (id, email, enabled, first_name, last_name, password, role) VALUES (1003, 'paul@johnabbottcollege.net', 1, 'Paul', 'Cheung', '$2a$10$rFK3.d/QbOaOGLxWweuimuob6om5j0PKeJOShGBrFLOjcfVwmUUV.','ADMIN');
+INSERT INTO dev2gether.user (id, email, enabled, first_name, last_name, password, role) VALUES (1004, 'eric@johnabbottcollege.net', 1, 'Eric', 'Hamelin', '$2a$10$rFK3.d/QbOaOGLxWweuimuob6om5j0PKeJOShGBrFLOjcfVwmUUV.','ADMIN');
 
 
---INSERT INTO dev2gether.language
---(id, name)
---VALUES
---(1 , 'Java'),
---(2 , 'SQL'),
---(3 , 'HTML'),
---(4 , 'CSS'),
---(5 , 'JavaScript'),
---(6 , 'PHP'),
---(7 , 'Python'),
---(8 , 'Go'),
---(9 , `C#`);
+INSERT INTO dev2gether.language (id, name) VALUES (1 , 'Java');
+INSERT INTO dev2gether.language (id, name) VALUES (2 , 'PHP');
+INSERT INTO dev2gether.language (id, name) VALUES (3 , 'Javascript');
+INSERT INTO dev2gether.language (id, name) VALUES (4 , 'C');
+INSERT INTO dev2gether.language (id, name) VALUES (5 , 'Python');
+INSERT INTO dev2gether.language (id, name) VALUES (6 , 'CSS');
+INSERT INTO dev2gether.language (id, name) VALUES (7 , 'HTML');
+INSERT INTO dev2gether.language (id, name) VALUES (8 , 'React');
+INSERT INTO dev2gether.language (id, name) VALUES (9 , 'Laravel');
+
+
+
